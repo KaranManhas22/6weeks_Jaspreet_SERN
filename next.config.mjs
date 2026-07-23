b91f63a28ee83f3d62c79a5eb57296d0fd56c2c8
Jaspreet Bhatia <code4youbuddy@gmail.com>
2026-07-23 20:42:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: ['18.143.172.203:3000'],
+    allowedDevOrigins: ['18.143.172.203:3000', 'foodzie.store', 'www.foodzie.store'],
   },
   images: {
     unoptimized: true,
